fd072edf35263d6753136607c915120ce93350af
lihailin <lihailin@xiaomi.com>
2021-06-01 13:14:34 +0800

    add merge sort algorithm

--- sort/merge_sort.c
+++ sort/merge_sort.c
@@ -1,7 +1,7 @@
 /*================================================================
 *   Copyright (C) 2021 Xiaomi. All rights reserved.
 *   
-*   文件名称：quick_sort.c
+*   文件名称：merge_sort.c
 *   创 建 者：LiHailin
 *   创建日期：2021年05月20日
 *   描    述：
@@ -13,22 +13,18 @@
 #include <algorithm>
 using namespace std;
 
-// [)
-void quick_sort(vector<int>& nums, int i, int j) {
-    if (i >= j) return;
-
+// []
+void merge_sort(vector<int>& nums, int i, int j) {
+    if (i == j) return;
     int mid = (i + j) >> 1;
-    auto ppos = partition(nums.begin() + i, nums.begin() + j, [nums, mid](const int num) {
-            if (num < nums[mid]) return true; 
-            return false;
-            });
-    int posOffset = ppos - nums.begin();
-    quick_sort(nums, i, posOffset);
-    quick_sort(nums, posOffset + 1, j);
+
+    merge_sort(nums, i, mid);
+    merge_sort(nums, mid+1, j);
+    inplace_merge(nums.begin()+i, nums.begin()+mid+1, nums.begin()+j+1);
 }
 
-void quick_sort(vector<int>& nums) {
-    quick_sort(nums, 0, nums.size());
+void merge_sort(vector<int>& nums) {
+    merge_sort(nums, 0, nums.size()-1);
 }
 
 void show(vector<int>& nums) {
@@ -40,9 +36,9 @@ void show(vector<int>& nums) {
 
 int main() {
     vector<int> nums{1,3,465,22,456,213,7675,1232};
-    nums = {1,3};
-    nums = {1};
-    quick_sort(nums);
+    //nums = {1,3};
+    //nums = {1};
+    merge_sort(nums);
     show(nums);
 
     return 0;
