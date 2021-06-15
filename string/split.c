4c4272116158927a2209df9b92641ec6a9ecf770
lihailin <lihailin@xiaomi.com>
2021-06-15 10:23:01 +0800

    add string split func

---
+++ string/split.c
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+// 该分割函数，不能分割 separator 个数为0的情况
+vector<string> split(const string& s, const char& separator) {
+    vector<string> res;
+    int beginPos = 0;
+    int desPos = s.npos;
+    while ((desPos = s.find(separator, beginPos)) 
+            && desPos != s.npos) {
+        res.push_back(s.substr(beginPos, desPos-beginPos));
+        beginPos = desPos + 1;
+    }
+    return res;
+}
+
+int main() {
+    string test("sfsg.sfsf.hgt.rhrha.sdeyet.eeyrtj.dghd");
+    test = "sfsg";
+    test = "";
+    auto splitRes = split(test, '.');
+    for (const string& s: splitRes) {
+        cout << s << " ";
+    }
+    cout << endl;
+    return 0;
+}
